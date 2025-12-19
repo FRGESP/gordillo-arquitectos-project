61ef737f8f79ccb74f2e11eecfca5d8ab0c350f3
FRGESP <f.gordilloespinoza@ugto.mx>
2025-12-18 21:54:59 -0600

    SEO

--- src/app/servicios/page.tsx
+++ src/app/servicios/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { redirect } from "next/navigation";
 function Serviciospage() {
-  redirect('/');
+  redirect('/servicios/construccion');
 }
 
 export default Serviciospage
